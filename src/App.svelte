cadf7b3aeba7fc55b9813ce0da3914402793ecfe
S.Khalezov <energet666@gmail.com>
2025-06-20 15:59:37 +0500

    add buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	//const host = window.location.hostname;
-	// const apiHost = "http://" + host + ":8080"; //для отладки или если статика и бэкенд на разных хостах
-	const apiHost = "";
+	import { Power, Save } from "@lucide/svelte";
+	const host = window.location.hostname;
+	const protocol = window.location.protocol;
+	const apiHost = protocol + "//" + host + ":8080"; //для отладки или если статика и бэкенд на разных хостах
+	// const apiHost = "http://127.0.0.1:8080";
 
 	window.addEventListener("unload", () => {
 		console.log("unload");
@@ -9,6 +11,8 @@
 	});
 
 	let count = $state(0);
+
+	let powerIsActive = $state(false);
 
 	type Msg = {
 		Count: number;
@@ -34,7 +38,57 @@
 			<p class="">Loads: {count}</p>
 		</div>
 	</div>
+	<div
+		class="my-block p-3 bg-primary-500 rounded-full flex gap-4 text-white/80"
+	>
+		<button
+			class="my-button p-2 rounded-full inline-block {powerIsActive
+				? 'my-button-active'
+				: ''}"
+			onclick={() => {
+				powerIsActive = !powerIsActive;
+			}}
+		>
+			<Power />
+		</button>
+		<div class="my-button p-2 rounded-full inline-block">
+			<Save />
+		</div>
+		<!-- <input
+			type="checkbox"
+			class="my-checkbox"
+		/> -->
+	</div>
 </main>
 
 <style lang="postcss">
+	.my-button {
+		cursor: pointer;
+		/* border: solid 2px transparent; */
+		transition: all 0.1s ease-in-out;
+		background-image: linear-gradient(to left top, #00000030, #ffffff30);
+		/* background-repeat: repeat; */
+		background-size: 125% 125%;
+		background-position: center;
+		border: solid 3px transparent;
+		/* background-clip: border-box; */
+		box-shadow:
+			4px 4px 7px 0px rgb(0 0 0 / 25%),
+			-4px -4px 7px 0px rgb(255 255 255 / 25%);
+	}
+	.my-block {
+		box-shadow:
+			inset 4px 4px 7px 0px rgb(0 0 0 / 25%),
+			inset -4px -4px 7px 0px rgb(255 255 255 / 25%);
+	}
+	.my-button-active {
+		box-shadow:
+			inset 2px 1px 0px 0px rgb(0 0 0 / 0%),
+			inset -3px -3px 5px 0px rgb(255 255 255 / 26%),
+			inset 3px 3px 8px -3px #0000007d;
+		background-image: none;
+		color: greenyellow;
+		border-color: greenyellow;
+		background-clip: padding-box;
+	}
 </style>
